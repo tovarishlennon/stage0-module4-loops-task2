bcb2c53e497e07f011fabba6bfb2dcf277ff5714
Azim Shamuratov <lennon645@yandex.com>
2022-09-14 11:52:05 +0500

    Task #2. While Loops

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int counter = 0;
+        int multipliedNumbers = multiplyByAndToInclusive > 0 ? multiplyByAndToInclusive * multiplyByAndToInclusive : multiplyByAndToInclusive * (-multiplyByAndToInclusive);
+
+        if (multipliedNumbers > 0) {
+            while (counter <= multipliedNumbers) {
+                System.out.println(counter);
+                counter += multiplyByAndToInclusive;
+            }
+        } else if (multipliedNumbers < 0) {
+            while ( counter >= multipliedNumbers) {
+                System.out.println(counter);
+                counter += multiplyByAndToInclusive;
+            }
+        } else {
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 0;
+        if (printToInclusive % 2 == 0) {
+            while (counter <= printToInclusive) {
+                System.out.println(counter);
+                counter++;
+            }
+        } else {
+            while ( counter <= printToInclusive) {
+
+            }
+        }
+
     }
 }
